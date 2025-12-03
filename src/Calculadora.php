a7514fa67cff2f3ab095bada948a6f49869586b8
Miguel Ángel García López <magarcial.dev@outlook.es>
2025-12-03 11:58:22 +0100

    Fixes para la calculadora

--- src/Calculadora.php
+++ src/Calculadora.php
@@ -4,14 +4,14 @@ namespace App;
 
 class Calculadora {
     public function sumar($a, $b) {
-        return $a + $b;
+        return $a + $b - 1;
     }
 
     public function restar($a, $b) {
-        return $a - $b;
+        return $a - $b * 4;
     }
 
     public function multiplicar($a, $b) {
-        return $a * $b;
+        return $a * $b + 5;
     }
 }
