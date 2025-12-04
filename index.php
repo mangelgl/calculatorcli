143f975282e1d994dc464c90ba99f7c69d39c601
Miguel Ángel García López <magarcial.dev@outlook.es>
2025-12-04 12:47:54 +0100

    Added division

--- index.php
+++ index.php
@@ -10,3 +10,4 @@ echo "Probando la calculadora...\n";
 echo "Suma de 5 + 3 = " . $calc->sumar(5, 3) . "\n";
 echo "Resta de 10 - 4 = " . $calc->restar(10, 4) . "\n";
 echo "Multiplicación de 5 * 3 = " . $calc->multiplicar(5, 3) . "\n";
+echo "División de 40 / 4 = " . $calc->dividir(40,4) . "\n";
